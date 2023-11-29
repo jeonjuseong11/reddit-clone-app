e25f01e3200aee76c5ff8f2b05e05a2b34ab9670
jeonjuseong11 <111875481+jeonjuseong11@users.noreply.github.com>
2023-11-29 13:03:51 +0900

    Feat : register
    회원가입 폼, api 기본 추가

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,21 @@
 import express from "express";
 import morgan from "morgan";
 import { AppDataSource } from "./data-source";
-
+import authRoute from "./routes/auth";
+import cors from "cors";
 const app = express();
-
+const origin = "http://localhost:3000";
+app.use(
+  cors({
+    origin,
+  })
+);
 app.use(express.json());
 app.use(morgan("dev")); //dev, short, common, combined 중 개발 환경에서는 dev를 가장 많이 씀
 app.get("/", (_, res) => res.send("runnging")); //url로 접속을 하면 해당 블록의 코드를 실행
 
 let port = 4000; //포트를 지정
+app.use("/api/auth", authRoute);
 app.listen(port, async () => {
   //express를 실행할때 실행 후 로그를 보여준다
   console.log(`Server running at http://localhost:${port}`);
