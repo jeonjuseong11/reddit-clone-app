582120b7d87b5a5d6ced7f01c0131944717f580b
jeonjuseong11 <111875481+jeonjuseong11@users.noreply.github.com>
2024-01-16 22:25:19 +0900

    Feat : Middleware
    auth user subs middleware 생성

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import morgan from "morgan";
 import { AppDataSource } from "./data-source";
-import authRoute from "./routes/auth";
+import authRoutes from "./routes/auth";
+import subRoutes from "./routes/auth";
+
 import cors from "cors";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 const app = express();
 const origin = "http://localhost:3000";
 app.use(
@@ -14,13 +17,16 @@ app.use(
 );
 app.use(express.json());
 app.use(morgan("dev")); //dev, short, common, combined 중 개발 환경에서는 dev를 가장 많이 씀
+app.use(cookieParser()); //쿠키를 받을 수 있음
 
 dotenv.config();
 
 app.get("/", (_, res) => res.send("runnging")); //url로 접속을 하면 해당 블록의 코드를 실행
 
 let port = 4000; //포트를 지정
-app.use("/api/auth", authRoute);
+app.use("/api/auth", authRoutes);
+app.use("/api/subs", subRoutes);
+
 app.listen(port, async () => {
   //express를 실행할때 실행 후 로그를 보여준다
   console.log(`Server running at http://localhost:${port}`);
